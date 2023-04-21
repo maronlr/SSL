f62f37cfee5c55d140c3987a03dded299cb56029
Maron Leonel Rabahia <mrabahia@frba.utn.edu.ar>
2023-04-21 19:31:43 -0300

    Hola Mundo a archivo txt

--- 00-CHelloworld/hello.c
+++ 00-CHelloworld/hello.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main(){
+    FILE *output;
+    output = fopen("output.txt", "w");
     fprintf(stdout, "Hello, World!");
+    fprintf(output, "Hello, World!");
     return 0;
 }
